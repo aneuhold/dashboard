21a3de5bc7154f2196c429c948f1d89534f2682a
Anton G Neuhold Jr <agneuhold@gmail.com>
2024-01-24 15:03:07 -0800

    Add some nice tag sorting

--- src/components/Tasks/TaskList/TaskList.svelte
+++ src/components/Tasks/TaskList/TaskList.svelte
@@ -4,7 +4,11 @@
   A list of tasks.
 -->
 <script lang="ts">
-  import { getDefaultTaskListSortSettings } from '@aneuhold/core-ts-db-lib';
+  import {
+    DashboardTaskService,
+    DashboardTaskSortBy,
+    getDefaultTaskListSortSettings
+  } from '@aneuhold/core-ts-db-lib';
   import TaskRow from 'components/Tasks/TaskRow.svelte';
   import { flip } from 'svelte/animate';
   import { slide } from 'svelte/transition';
@@ -17,6 +21,7 @@
   export let category: string;
   export let parentTaskId: string | undefined = undefined;
 
+  const taskMap = TaskMapService.getStore();
   $: parentTask = parentTaskId ? TaskMapService.getTaskStore(parentTaskId) : undefined;
   $: parentTaskSortSettings = $parentTask ? $parentTask.sortSettings[$currentUserId] : undefined;
   $: userTaskSortSettings = $userSettings.config.taskListSortSettings[category];
@@ -24,6 +29,18 @@
     parentTaskSortSettings ??
     userTaskSortSettings ??
     getDefaultTaskListSortSettings($currentUserId);
+  $: isSortedByTagsFirst =
+    currentSortSettings.sortList.length !== 0 &&
+    currentSortSettings.sortList[0].sortBy === DashboardTaskSortBy.tags;
+  $: tagHeaderMap = isSortedByTagsFirst
+    ? DashboardTaskService.getTagHeaderMap(
+        $taskMap,
+        taskIds,
+        $currentUserId,
+        $userSettings.config.tagSettings,
+        'No Priority'
+      )
+    : undefined;
 </script>
 
 <div class="content">
@@ -36,7 +53,10 @@
   />
   {#each taskIds as taskId (taskId)}
     <div transition:slide animate:flip={{ duration: 200 }}>
-      <TaskRow {taskId} />
+      <TaskRow
+        tagHeaderName={tagHeaderMap && tagHeaderMap[taskId] ? tagHeaderMap[taskId] : undefined}
+        {taskId}
+      />
     </div>
   {/each}
 </div>
--- src/components/Tasks/TaskTags/TaskRowTagHeader.svelte
+++ src/components/Tasks/TaskTags/TaskRowTagHeader.svelte
@@ -3,13 +3,19 @@
 </script>
 
 <div class="container">
-  <span>{tagName}</span>
+  <i class="tagHeader mdc-typography--body2 mdc-theme--text-hint-on-background">{tagName}</i>
 </div>
 
 <style>
   .container {
     width: 100%;
-    border-radius: 2px;
     padding: 4px;
+    display: flex;
+    flex-direction: row;
+  }
+  .tagHeader {
+    border-radius: 2px;
+    border-color: var(--mdc-theme-text-hint-on-background);
+    border-width: 1px;
   }
 </style>
