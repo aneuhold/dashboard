91ec22202bba652b02d7008ce705118995b980aa
Anton G Neuhold Jr <agneuhold@gmail.com>
2024-06-24 18:40:33 -0700

    Create new way to use confetti

--- src/components/presentational/Confetti/Confetti.svelte
+++ src/components/presentational/Confetti/Confetti.svelte
@@ -1,37 +1,93 @@
+<script lang="ts" context="module">
+  import { writable } from 'svelte/store';
+
+  export const triggerConfetti = (centerX: number, centerY: number, durationMs?: number) => {
+    confettiSettings.set({ centerX, centerY, durationMs, show: true });
+  };
+
+  type ConfettiSettings = {
+    centerX: number;
+    centerY: number;
+    durationMs?: number;
+    show: boolean;
+  };
+
+  function createConfettiSettings() {
+    let currentTimeout: NodeJS.Timeout | undefined = undefined;
+    const { subscribe, set } = writable<ConfettiSettings>({
+      centerX: 100,
+      centerY: 100,
+      show: false
+    });
+    return {
+      subscribe,
+      set: (settings: ConfettiSettings) => {
+        if (settings.show) {
+          if (currentTimeout) {
+            // Clear the timeout
+            clearTimeout(currentTimeout);
+            // Set the settings to not display the confetti anymore
+            set({ ...settings, show: false });
+            // Wait for the next tick to set the settings to display the
+            // confetti again
+            tick().then(() => {
+              set(settings);
+            });
+          } else {
+            set(settings);
+          }
+          currentTimeout = setTimeout(() => {
+            set({ ...settings, show: false });
+          }, settings.durationMs ?? 3000);
+        } else {
+          set(settings);
+        }
+      }
+    };
+  }
+  const confettiSettings = createConfettiSettings();
+</script>
+
+<!--
+  @component
+  
+  A component that provides a confetti animation to the rest of the application.
+  This is separated out to solve issues with overflow, and to make sure there
+  is ever only 1 animation happening at a time.
+
+  Maybe write something for the dispatch of confetti being complete?
+-->
 <script lang="ts">
   import { userSettings } from '$stores/userSettings/userSettings';
   import { confetti } from '@neoconfetti/svelte';
-  import { createEventDispatcher } from 'svelte';
-
-  /**
-   * Determines if the confetti should be shown. This should be bound to because
-   * it resets after the {@link durationMs} back to false.
-   */
-  export let show = false;
-  export let durationMs = 3000;
+  import { tick } from 'svelte';
 
   $: confettiEnabled = $userSettings.config.enabledFeatures.useConfettiForTasks;
-  $: showConfetti = show && confettiEnabled;
-
-  let currentTimeout: NodeJS.Timeout | undefined = undefined;
-
-  const dispatch = createEventDispatcher();
-
-  $: if (show && confettiEnabled) {
-    if (currentTimeout) clearTimeout(currentTimeout);
-    currentTimeout = setTimeout(() => {
-      show = false;
-      dispatch('confettiComplete');
-    }, durationMs);
-  }
+  $: showConfetti = $confettiSettings.show && confettiEnabled;
 </script>
 
 {#if showConfetti}
-  <div class="confettiContainer" use:confetti></div>
+  <div class="confettiContainer">
+    <div
+      class="confetti"
+      style={`left: ${$confettiSettings.centerX}px; top: ${$confettiSettings.centerY}px;`}
+      use:confetti={{
+        stageWidth: window.innerWidth,
+        stageHeight: window.innerHeight,
+        duration: $confettiSettings.durationMs ?? 3000
+      }}
+    ></div>
+  </div>
 {/if}
 
 <style>
   .confettiContainer {
     position: fixed;
+    pointer-events: none;
+    width: 100vw;
+    height: 100vh;
+  }
+  .confetti {
+    position: absolute;
   }
 </style>
--- src/components/presentational/Confetti/ConfettiExample.svelte
+++ src/components/presentational/Confetti/ConfettiExample.svelte
@@ -1,32 +1,23 @@
 <script lang="ts">
-  import Confetti from './Confetti.svelte';
+  import Confetti, { triggerConfetti } from './Confetti.svelte';
 
   export let numButtons = 1;
 
-  $: confettiButtonInfo = Array(numButtons)
-    .fill(null)
-    .map(() => ({
-      showConfetti: false
-    }));
-
-  $: {
-    console.log(confettiButtonInfo);
-  }
+  $: confettiButtonInfo = Array(numButtons).fill(null);
 </script>
 
 <div class="exampleContainer">
-  {#each confettiButtonInfo as confettiInfo}
-    <div class="confettiContainer">
-      <Confetti bind:show={confettiInfo.showConfetti} />
-      <button
-        class="exampleConfettiButton"
-        on:click={() => {
-          confettiInfo.showConfetti = true;
-        }}
-      >
-        Click me
-      </button>
-    </div>
+  <Confetti />
+  {#each confettiButtonInfo as _}
+    <button
+      class="exampleConfettiButton"
+      on:click={(event) => {
+        console.log('did it get here?');
+        triggerConfetti(event.clientX, event.clientY);
+      }}
+    >
+      Click me
+    </button>
   {/each}
 </div>
 
@@ -34,9 +25,8 @@
   .exampleContainer {
     display: flex;
     flex-direction: column;
-    align-items: center;
   }
   .exampleConfettiButton {
-    margin: auto;
+    margin-right: auto;
   }
 </style>
