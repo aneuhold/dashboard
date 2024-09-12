d26fdc1061ee77e4dc01429bf87f5312006fa353
Anton G Neuhold Jr <agneuhold@gmail.com>
2024-09-12 05:40:21 -0700

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,10 @@ import svelteConfig from '@aneuhold/eslint-config/src/svelte-config.js';
 export default [
   ...svelteConfig,
   {
-    // other override settings. e.g. for `files: ['**/*.test.*']`
+    rules: {
+      // Disabled because it seemed to be causing issues with a generic type
+      // that is used in an assertion `as type` at the end of a method
+      '@typescript-eslint/no-unnecessary-type-parameters': 'off'
+    }
   }
 ];
