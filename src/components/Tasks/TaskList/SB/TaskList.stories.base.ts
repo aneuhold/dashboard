3c41c242454feb84fc80fba7d9361505ad02469d
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-11-09 09:33:15 -0800

    Fix more stories

--- src/components/Tasks/TaskList/SB/TaskList.stories.base.ts
+++ src/components/Tasks/TaskList/SB/TaskList.stories.base.ts
@@ -15,12 +15,9 @@ const sbTaskListMetaBase: Parameters<typeof defineMeta>[0] = {
   title: 'Stateful Components/TaskList',
   component: SbTaskListExample,
   decorators: [
-    // @ts-expect-error - Decorator type mismatch between Storybook and Svelte - see https://github.com/storybookjs/storybook/issues/29951
-    () => SbConfettiDecorator,
-    // @ts-expect-error - Decorator type mismatch between Storybook and Svelte
-    () => SbSingletonTaskSharingDialogDecorator,
-    // @ts-expect-error - Decorator type mismatch between Storybook and Svelte
-    () => SbSingletonTaskAssignmentDialogDecorator
+    () => ({ Component: SbConfettiDecorator }),
+    () => ({ Component: SbSingletonTaskSharingDialogDecorator }),
+    () => ({ Component: SbSingletonTaskAssignmentDialogDecorator })
   ],
   argTypes: {
     sharedWith: createEnumArgType(MockTaskSharedWith),
