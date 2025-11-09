de4bf405faa1e2cff2c8f112a74c5177cd252d4c
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-11-09 09:20:16 -0800

    Update task list stories

---
+++ src/components/Tasks/TaskList/SB/TaskList.BasicUI.stories.svelte
@@ -0,0 +1,13 @@
+<script module lang="ts">
+  import sbTaskListMetaBase from '$components/Tasks/TaskList/SB/TaskList.stories.base';
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  const { Story } = defineMeta({
+    ...sbTaskListMetaBase,
+    title: 'Stateful Components/TaskList/Basic UI'
+  });
+</script>
+
+<Story name="Single Task" args={{ numTasks: 1 }} />
+<Story name="No Tasks" args={{ numTasks: 0 }} />
+<Story name="Multiple Tasks" />
---
+++ src/components/Tasks/TaskList/SB/TaskList.Dates.stories.svelte
@@ -0,0 +1,13 @@
+<script module lang="ts">
+  import sbTaskListMetaBase from '$components/Tasks/TaskList/SB/TaskList.stories.base';
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  const { Story } = defineMeta({
+    ...sbTaskListMetaBase,
+    title: 'Stateful Components/TaskList/Dates'
+  });
+</script>
+
+<Story name="Due Dates" args={{ includeDueDates: true }} />
+<Story name="Start Dates" args={{ includeStartDates: true }} />
+<Story name="Both Dates" args={{ includeDueDates: true, includeStartDates: true }} />
