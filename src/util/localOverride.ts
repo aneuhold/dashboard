a54645855ea11753058adb334c875c2b0783b871
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-11-25 18:15:13 -0800

    Put localoverride back to normal

--- src/util/localOverride.ts
+++ src/util/localOverride.ts
@@ -4,7 +4,7 @@ import { APIService } from '@aneuhold/core-ts-api-lib';
  * Overrides various things for local development if set to true.
  */
 export default function localOverride() {
-  const enableLocalOverride = true;
+  const enableLocalOverride = false;
   if (enableLocalOverride) {
     console.log('Local override active: Using local API URL');
     APIService.setAPIUrl('http://localhost:8080/');
