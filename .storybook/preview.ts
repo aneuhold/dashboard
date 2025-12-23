fe177d54524324564da7a1bd1efec4e28c97acf9
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-12-22 18:26:57 -0800

    Fix storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -18,6 +18,11 @@ console.warn = (...args) => {
   originalConsoleWarn(...args);
 };
 
+// This is called at the top level to ensure that global mocks are set up
+// before any other modules are loaded. This is important because some
+// modules have side effects that call the API when they are imported.
+TestSetup.setupGlobalMocks(spyOn);
+
 const preview: Preview = {
   beforeEach: () => {
     TestSetup.setupGlobalMocks(spyOn);
